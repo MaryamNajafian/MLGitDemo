21db334d023fcac19aec13aa42920fe8cd138c09
najafian <maryamnajafian@yahoo.com>
2017-12-19 13:47:40 -0500

    deleted many lines

--- helloWorld.py
+++ helloWorld.py
@@ -1,25 +1,5 @@
 #!/usr/bin/python 
  
-print "hlelloooooooooo"
-print "baaalaaa"
-print "blaaa"
+print "blabbbkaa"
 print "blaaa"
 print "hello world" 
-print "hello world" 
-print "hello world" 
-
-print "it works"
-
-print "Just making some small changes"
-print "shouldnt ruin anything right??"
-
-print "hi"
-
-print "git is the best"
-
-print "hi"
-
-print "I hope no one breaks this"
-print "I hope no one breaks this"
-print "I hope no one breaks this"
-print "I hope no one breaks this"
