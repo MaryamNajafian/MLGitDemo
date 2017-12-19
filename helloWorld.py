7204fbf9088f24aabc5470f19db72753a8599aa6
najafian <najafian@dhcp-18-111-12-48.dyn.mit.edu>
2017-12-19 13:18:15 -0500

    joojoo

--- helloWorld.py
+++ helloWorld.py
@@ -7,6 +7,11 @@ print "it works"
 
 print "Just making some small changes"
 print "shouldnt ruin anything right??"
+
+print "hi"
+
 print "git is the best"
+
+print "hi"
 
 print "I hope no one breaks this"
